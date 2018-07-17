902ba84a78cbf496cad451fab4a8bf4d123922e1
PonySchreck <max98tonton@gmail.com>
2018-07-18 01:50:02 +0200

    implemented Atomic Integer and deprecated Decorator

---
+++ PokemonAdventure/AtomicInteger.py
@@ -0,0 +1,31 @@
+import threading
+
+
+class AtomicInteger:
+    """
+    Thread safe integer class
+    """
+
+    def __init__(self, value=0):
+        self._value = value
+        self._lock = threading.Lock()
+
+    def inc(self):
+        with self._lock:
+            self._value += 1
+            return self._value
+
+    def dec(self):
+        with self._lock:
+            self._value -= 1
+            return self._value
+
+    @property
+    def value(self):
+        with self._lock:
+            return self._value
+
+    @value.setter
+    def value(self, v):
+        with self._lock:
+            self._value = v
